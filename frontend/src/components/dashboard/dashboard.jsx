246215dce65ac0c40917e55b2f8dd29e6cc75b36
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-11-05 21:21:42 -0300

    Criando um grid e fazendo parte do layout da pagina Dashboard

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
 import Content from '../layout/content'
 import ContentHeader from '../layout/contentHeader'
+import ValueBox from '../widget/valueBox'
+import Row from '../layout/row'
 
 
 export default function Dashboard(props){
     return(
         <div>
-            <ContentHeader title='Dashboard' small='Versão 1'/>
+            <ContentHeader 
+                title='Dashboard' 
+                small='Versão 1'/>
             <Content>
-                Dashboard
+                <Row>
+                    <ValueBox 
+                        cols='12 4' 
+                        color='green' 
+                        icon='bank' 
+                        value='R$ 10' 
+                        text='Total de Creditos' />
+                    <ValueBox 
+                        cols='12 4' 
+                        color='red' 
+                        icon='credit-card' 
+                        value='R$ 10' 
+                        text='Total de Débitos' />
+                    <ValueBox 
+                        cols='12 4' 
+                        color='blue' 
+                        icon='money' 
+                        value='R$ 10' 
+                        text='Valor Consolidado' />
+                </Row>
             </Content>
         </div>
     )
