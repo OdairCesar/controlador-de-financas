95a5865ce8eeafa3e3d2b0b8dd7fe2602a02bb67
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-11-07 02:40:29 -0300

    Criando e configurando o react-redux

--- frontend/src/components/pages/dashboard/dashboard.jsx
+++ frontend/src/components/pages/dashboard/dashboard.jsx
@@ -1,38 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import { getSummary } from './dashboardActions'
 import Content from '../../template/content'
 import ContentHeader from '../../template/contentHeader'
 import ValueBox from '../../widget/valueBox'
 import Row from '../../layout/row'
 
 
-export default function Dashboard(props){
-    return(
-        <div>
-            <ContentHeader 
-                title='Dashboard' 
-                small='Versão 1'/>
-            <Content>
-                <Row>
-                    <ValueBox 
-                        cols='12 4' 
-                        color='green' 
-                        icon='bank' 
-                        value='R$ 10' 
-                        text='Total de Creditos' />
-                    <ValueBox 
-                        cols='12 4' 
-                        color='red' 
-                        icon='credit-card' 
-                        value='R$ 10' 
-                        text='Total de Débitos' />
-                    <ValueBox 
-                        cols='12 4' 
-                        color='blue' 
-                        icon='money' 
-                        value='R$ 10' 
-                        text='Valor Consolidado' />
-                </Row>
-            </Content>
-        </div>
-    )
+class Dashboard extends Component{
+
+    componentWillMount(){
+        this.props.getSummary()
+    }
+    
+    render(){
+        const { credit, debt } = this.props.summary
+        return(
+            <div>
+                <ContentHeader 
+                    title='Dashboard' 
+                    small='Versão 1'/>
+                <Content>
+                    <Row>
+                        <ValueBox 
+                            cols='12 4' 
+                            color='green' 
+                            icon='bank' 
+                            value={`R$ ${credit}`}
+                            text='Total de Creditos' />
+                        <ValueBox 
+                            cols='12 4' 
+                            color='red' 
+                            icon='credit-card' 
+                            value={`R$ ${debt}`} 
+                            text='Total de Débitos' />
+                        <ValueBox 
+                            cols='12 4' 
+                            color='blue' 
+                            icon='money' 
+                            value={`R$ ${credit - debt}`} 
+                            text='Valor Consolidado' />
+                    </Row>
+                </Content>
+            </div>
+        )
+    }
 }
+
+const mapStateToProps = state =>({ summary: state.dashboard.summary })
+const mapDispatchToProps = dispatch => bindActionCreators( {getSummary}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps) (Dashboard)
