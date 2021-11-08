f32f82254f9a0777ae6c5b04ffa3bde316cd02c6
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-11-08 09:21:35 -0300

    Criando action e reducer para os componentes de abas e os conectando ao estado da aplicação no redux

--- frontend/src/main/reducers.jsx
+++ frontend/src/main/reducers.jsx
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import DashboardReducer from '../components/pages/dashboard/dashboardReducer'
+import TabReducer from '../components/layout/tab/tabReducer'
 
 const rootReducer = combineReducers({
-    dashboard: DashboardReducer
+    dashboard: DashboardReducer,
+    tab: TabReducer
 })
 
 export default rootReducer
