0031264099cf692e06221a21aa675072cc557cf3
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-11-08 08:52:22 -0300

    Criando a estrutura inicial das abas

--- frontend/src/components/pages/billingCycles/billingCycles.jsx
+++ frontend/src/components/pages/billingCycles/billingCycles.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import ContentHeader from '../../template/contentHeader'
 import Content from '../../template/content'
+import Tabs from '../../layout/tab/tabs'
+import TabsHeader from '../../layout/tab/tabsHeader'
+import TabsContent from '../../layout/tab/tabsContent'
+import TabHeader from '../../layout/tab/tabHeader'
 
 class BillingCycles extends Component{
     render(){
@@ -8,7 +12,17 @@ class BillingCycles extends Component{
             <div>
                 <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
                 <Content>
-                    Ciclo de Pagamento
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='Listar' icon='bars' target='tabList'/>
+                            <TabHeader label='Incluir' icon='plus' target='tabCreate'/>
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate'/>
+                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete'/>
+                        </TabsHeader>
+                        <TabsContent>
+
+                        </TabsContent>
+                    </Tabs>
                 </Content>
             </div>
         )
