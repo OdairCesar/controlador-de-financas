eba481b3821a0b7fc7e04d8a901d0a3c871cb141
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-11-09 13:22:02 -0300

    Começando a criar o corpo de cada aba criada.

--- frontend/src/components/pages/billingCycles/billingCycles.jsx
+++ frontend/src/components/pages/billingCycles/billingCycles.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
 import ContentHeader from '../../template/contentHeader'
 import Content from '../../template/content'
 import Tabs from '../../layout/tab/tabs'
 import TabsHeader from '../../layout/tab/tabsHeader'
-import TabsContent from '../../layout/tab/tabsContent'
 import TabHeader from '../../layout/tab/tabHeader'
+import TabsContent from '../../layout/tab/tabsContent'
+import TabContent from '../../layout/tab/tabContent'
+import { selectTab } from '../../layout/tab/tabActions'
 
 class BillingCycles extends Component{
+
+    componentWillMount(){
+        this.props.selectTab('tabList')
+    }
+
     render(){
         return(
             <div>
@@ -20,7 +30,10 @@ class BillingCycles extends Component{
                             <TabHeader label='Excluir' icon='trash-o' target='tabDelete'/>
                         </TabsHeader>
                         <TabsContent>
-
+                            <TabContent id='tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                            <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                         </TabsContent>
                     </Tabs>
                 </Content>
@@ -28,4 +41,6 @@ class BillingCycles extends Component{
         )
     }
 }
-export default BillingCycles
+
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+export default connect(null, mapDispatchToProps) (BillingCycles)
