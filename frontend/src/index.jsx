08b441c1fa74fa7e32877ecded62ec65f72b9b9e
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-11-29 02:15:44 -0300

    Fazendo a pagina de login aparecer até o token ser validado pela API

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -7,14 +7,14 @@ import promise from 'redux-promise'
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
 
-import App from './main/app'
+import AuthOrApp from './main/AuthOrApp'
 import reducers from './main/reducers'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <AuthOrApp />
     </Provider>,
     document.getElementById('app')
 )
