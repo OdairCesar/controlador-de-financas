21472bd8353af3ce3450abfca0478d47396f1372
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-11-29 02:13:28 -0300

    Criando o estado da pagina de autenticação no redux

--- frontend/src/components/pages/billingCycles/billingCycleActions.js
+++ frontend/src/components/pages/billingCycles/billingCycleActions.js
@@ -2,8 +2,9 @@ import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
 import { initialize} from 'redux-form'
 import { showTabs, selectTab } from './tab/tabActions'
+import consts from '../../../consts'
 
-const BASE_URL = 'http://localhost:3003/api'
+const BASE_URL = consts.API_URL
 const INITIAL_VALUES = { credits: [{}], debts: [{}]}
 
 export function getList(){
--- frontend/src/main/reducers.jsx
+++ frontend/src/main/reducers.jsx
@@ -5,13 +5,15 @@ import { reducer as toastrReducer } from 'react-redux-toastr'
 import DashboardReducer from '../components/pages/dashboard/dashboardReducer'
 import TabReducer from '../components/pages/billingCycles/tab/tabReducer'
 import BillingCycleReducer from '../components/pages/billingCycles/billingCycleReducer'
+import AuthReducer from '../components/pages/auth/AuthReducer'
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
     tab: TabReducer,
     billingCycle: BillingCycleReducer,
     form: formReducer,
-    toastr: toastrReducer
+    toastr: toastrReducer,
+    auth: AuthReducer
 })
 
 export default rootReducer
